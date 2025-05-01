a9d515b6b06fbbd53fc09cbe27f18931a8555804
sggnology <sggnology@gmail.com>
2025-05-01 19:00:30 +0900

    [master] server: cors 설정하지 않는 이유 작성

--- server/src/main/kotlin/com/sggnology/server/config/WebConfig.kt
+++ server/src/main/kotlin/com/sggnology/server/config/WebConfig.kt
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
+        // 현재는 서버가 react 를 포워딩해주고 있기에 cors 가 발생하지 않는다.
 //        registry.addMapping("/api/**").allowedOrigins("*")
     }
 }
