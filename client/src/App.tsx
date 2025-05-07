6f3d26f6f00cfd13940e7ef557dced811b79045d
sggnology <sggnology@gmail.com>
2025-05-07 17:37:39 +0900

    [master] client: 등록으로 이동을 위한 FloatingButton 생성

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,37 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Detail from './pages/Detail';
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/detail/:id" element={<Detail />} />
-      </Routes>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/detail/:id" element={<Detail />} />
+        </Routes>
+        <FloatingButton />
+      </div>
     </Router>
+  );
+}
+
+function FloatingButton() {
+  const navigate = useNavigate();
+
+  const handleFloatingButtonClick = () => {
+    navigate('/register');
+  };
+
+  return (
+    <button className="floating-button" onClick={handleFloatingButtonClick}>
+      +
+    </button>
   );
 }
 
