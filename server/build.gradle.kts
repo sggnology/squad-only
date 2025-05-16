0dccae79507266b8d967c119d7413eebb708b360
sggnology <sggnology@gmail.com>
2025-05-16 16:19:28 +0900

    [master] server: querydsl, openapi 의존성 추가

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,9 +1,12 @@
 plugins {
     kotlin("jvm") version "1.9.25"
+
+    kotlin("kapt") version "1.9.25" // !!! Required for annotation processing in Kotlin !!!
+    kotlin("plugin.jpa") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
+
     id("org.springframework.boot") version "3.4.5"
     id("io.spring.dependency-management") version "1.1.7"
-    kotlin("plugin.jpa") version "1.9.25"
 }
 
 group = "com.sggnology"
@@ -19,12 +22,25 @@ repositories {
     mavenCentral()
 }
 
+val queryDslVersion = "5.0.0" // Use a compatible QueryDSL version
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // QueryDSL Dependencies
+    implementation("com.querydsl:querydsl-jpa:$queryDslVersion:jakarta") // Use jakarta for Spring Boot 3+
+    implementation("com.querydsl:querydsl-core:$queryDslVersion")
+
+    // !!! Annotation Processor for QueryDSL !!!
+    kapt("com.querydsl:querydsl-apt:$queryDslVersion:jakarta") // Use jakarta for Spring Boot 3+
+
+    // springdoc-openapi 의존성 추가 (Swagger UI 포함)
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8")
+
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
