07657ed40f0715fdaf54036cc05a5a7b98f6c0f2
sggnology <sggnology@gmail.com>
2025-05-17 19:49:30 +0900

    [master] server: 컨텐츠 등록시 이미지 관리되게끔 수정

--- server/src/main/kotlin/com/sggnology/server/feature/content/registration/service/ContentRegistrationService.kt
+++ server/src/main/kotlin/com/sggnology/server/feature/content/registration/service/ContentRegistrationService.kt
@@ -5,6 +5,9 @@ import com.sggnology.server.db.sql.entity.ContentTagInfo
 import com.sggnology.server.db.sql.repository.ContentInfoRepository
 import com.sggnology.server.db.sql.repository.TagInfoRepository
 import com.sggnology.server.feature.content.registration.data.dto.ContentRegistrationReqDto
+import com.sggnology.server.feature.file.upload.data.model.FileAttachToContentModel
+import com.sggnology.server.feature.file.upload.data.model.FileMoveToStorageModel
+import com.sggnology.server.feature.file.upload.service.FileUploadService
 import com.sggnology.server.feature.tag.data.dto.TagRegistrationReqDto
 import com.sggnology.server.feature.tag.domain.TagManager
 import org.springframework.stereotype.Service
@@ -13,7 +16,9 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class ContentRegistrationService(
     private val contentInfoRepository: ContentInfoRepository,
-    private val tagInfoRepository: TagInfoRepository
+    private val tagInfoRepository: TagInfoRepository,
+
+    private val fileUploadService: FileUploadService
 ) {
 
     @Transactional
@@ -25,11 +30,24 @@ class ContentRegistrationService(
         val tagRegistrationReqDto = TagRegistrationReqDto(tagNames = registrationReqDto.tags)
         val tagRegistrationModel = tagManager.command(tagRegistrationReqDto)
 
+        val newFileInfos = fileUploadService.execute(
+            FileMoveToStorageModel(
+                fileIdxs = registrationReqDto.newFileIds
+            )
+        )
+
         // TODO: 이미지 연결, 등록자 등록
         val newContentInfo = ContentInfo(
             title = registrationReqDto.title,
             description = registrationReqDto.title,
             location = registrationReqDto.location,
+        )
+
+        fileUploadService.execute(
+            FileAttachToContentModel(
+                fileInfos = newFileInfos,
+                contentInfo = newContentInfo
+            )
         )
 
         newContentInfo.contentTags.addAll(
