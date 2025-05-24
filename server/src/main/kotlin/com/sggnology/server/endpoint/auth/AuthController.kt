7fc3bb36375d20080941e895bce37e0b8cfb3ea1
sggnology <sggnology@gmail.com>
2025-05-24 19:46:10 +0900

    [master] server: 로그인 endpoint swagger 설명 추가

--- server/src/main/kotlin/com/sggnology/server/endpoint/auth/AuthController.kt
+++ server/src/main/kotlin/com/sggnology/server/endpoint/auth/AuthController.kt
@@ -3,6 +3,10 @@ package com.sggnology.server.endpoint.auth
 import com.sggnology.server.feature.auth.data.dto.req.AuthLoginReqDto
 import com.sggnology.server.feature.auth.data.dto.res.AuthLoginResDto
 import com.sggnology.server.feature.auth.service.AuthService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,10 +14,17 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("api/v1/auth")
+@Tag(name = "인증 API")
 class AuthController(
     private val authService: AuthService
 ) {
 
+    @Operation(
+        summary = "로그인"
+    )
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "로그인 성공", useReturnTypeSchema = true),
+    ])
     @PostMapping("")
     fun login(
         @RequestBody authLoginReqDto: AuthLoginReqDto
