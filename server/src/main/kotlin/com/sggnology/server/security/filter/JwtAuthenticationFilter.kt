2f4774e43fb76d94dfeb4deb4ed99f94c6880794
sggnology <sggnology@gmail.com>
2025-05-26 18:09:48 +0900

    [master] server: JwtAuthenticationFilter 에서 resolveToken 을 Util 로 빼서 구성

--- server/src/main/kotlin/com/sggnology/server/security/filter/JwtAuthenticationFilter.kt
+++ server/src/main/kotlin/com/sggnology/server/security/filter/JwtAuthenticationFilter.kt
@@ -1,11 +1,11 @@
 package com.sggnology.server.security.filter
 
 import com.sggnology.server.security.JwtTokenProvider
+import com.sggnology.server.util.JwtTokenUtil
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.util.StringUtils
 import org.springframework.web.filter.OncePerRequestFilter
 
 class JwtAuthenticationFilter(
@@ -17,7 +17,7 @@ class JwtAuthenticationFilter(
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
-        val token = resolveToken(request)
+        val token = JwtTokenUtil.resolveToken(request)
 
         if (token != null && jwtTokenProvider.validateToken(token)) {
             val authentication = jwtTokenProvider.getAuthentication(token)
@@ -25,12 +25,5 @@ class JwtAuthenticationFilter(
         }
 
         filterChain.doFilter(request, response)
-    }
-
-    private fun resolveToken(request: HttpServletRequest): String? {
-        val bearerToken = request.getHeader("Authorization")
-        return if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-            bearerToken.substring(7)
-        } else null
     }
 }
---
+++ server/src/main/kotlin/com/sggnology/server/util/JwtTokenUtil.kt
@@ -0,0 +1,22 @@
+package com.sggnology.server.util
+
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.util.StringUtils
+
+object JwtTokenUtil {
+
+    fun resolveToken(request: HttpServletRequest): String? {
+        val bearerToken = request.getHeader("Authorization")
+        return resolveToken(bearerToken)
+    }
+
+    fun resolveToken(bearerToken: String?): String? {
+        return if (
+            bearerToken != null &&
+            StringUtils.hasText(bearerToken) &&
+            bearerToken.startsWith("Bearer ")
+        ) {
+            bearerToken.substring(7)
+        } else null
+    }
+}
