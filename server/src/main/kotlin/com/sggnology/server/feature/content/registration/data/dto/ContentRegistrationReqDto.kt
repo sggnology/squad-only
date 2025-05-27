3f0ffe4abaa36763b36ffda00af626b235538107
sggnology <sggnology@gmail.com>
2025-05-27 19:12:17 +0900

    [master] server: 컨탠츠 등록시 Valid 조건 추가

--- server/src/main/kotlin/com/sggnology/server/feature/content/registration/data/dto/ContentRegistrationReqDto.kt
+++ server/src/main/kotlin/com/sggnology/server/feature/content/registration/data/dto/ContentRegistrationReqDto.kt
@@ -1,13 +1,22 @@
 package com.sggnology.server.feature.content.registration.data.dto
 
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
 
 data class ContentRegistrationReqDto(
     val newFileIds: MutableSet<Long> = mutableSetOf(),
-    @field:NotBlank(message = "제목은 필수입니다.")
+
+    @field:NotBlank(message = "제목은 필수입니다")
+    @field:Size(max = 100, message = "제목은 100자 이하여야 합니다")
     val title: String,
-    @field:NotBlank(message = "설명은 필수입니다.")
+
+    @field:NotBlank(message = "설명은 필수입니다")
+    @field:Size(max = 1000, message = "설명은 1000자 이하여야 합니다")
     val description: String,
+
+    @field:NotBlank(message = "위치는 필수입니다")
+    @field:Size(max = 200, message = "위치는 200자 이하여야 합니다")
     val location: String,
+
     val tags: List<String>
 )
