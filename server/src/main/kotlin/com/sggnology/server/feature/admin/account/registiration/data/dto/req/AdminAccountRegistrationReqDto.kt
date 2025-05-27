f79ec6e95cef3f7d07ea3e4070ed7ab131e125e8
sggnology <sggnology@gmail.com>
2025-05-27 22:04:43 +0900

    [master] server: 관리자에서 사용자 추가시 dto 검증 로직 추가

--- server/src/main/kotlin/com/sggnology/server/feature/admin/account/registiration/data/dto/req/AdminAccountRegistrationReqDto.kt
+++ server/src/main/kotlin/com/sggnology/server/feature/admin/account/registiration/data/dto/req/AdminAccountRegistrationReqDto.kt
@@ -1,10 +1,17 @@
 package com.sggnology.server.feature.admin.account.registiration.data.dto.req
 
 import com.sggnology.server.feature.admin.account.registiration.data.model.AdminRegisterAccountModel
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
 
 data class AdminAccountRegistrationReqDto(
+    @field:NotBlank(message = "사용자 ID는 필수입니다")
+    @field:Size(min = 4, max = 20, message = "사용자 ID는 4자 이상 20자 이하여야 합니다")
     val userId: String,
-    val name: String
+
+    @field:NotBlank(message = "이름은 필수입니다")
+    @field:Size(max = 50, message = "이름은 50자 이하여야 합니다")
+    val name: String,
 ) {
     fun toAdminRegisterAccountModel(): AdminRegisterAccountModel {
         return AdminRegisterAccountModel(
