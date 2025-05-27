b0b3575452711c8b71ad7afd7bcea5beb4229084
sggnology <sggnology@gmail.com>
2025-05-27 17:55:20 +0900

    [master] server: 오류 발생시 로깅

--- server/src/main/kotlin/com/sggnology/server/exception/handler/GlobalExceptionHandler.kt
+++ server/src/main/kotlin/com/sggnology/server/exception/handler/GlobalExceptionHandler.kt
@@ -1,5 +1,6 @@
 package com.sggnology.server.exception.handler
 
+import com.sggnology.server.util.logger
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -16,11 +17,14 @@ class GlobalExceptionHandler {
             errors[error.field] = error.defaultMessage ?: "유효하지 않은 값입니다"
         }
 
+        logger.error("Validation errors: $errors")
+
         return ResponseEntity.badRequest().body(errors)
     }
 
     @ExceptionHandler(Exception::class)
     fun handleException(ex: Exception): ResponseEntity<String> {
+        logger.error("Unhandled exception occurred", ex)
         return ResponseEntity.badRequest().body(ex.message)
     }
 }
