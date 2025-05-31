6f833871f2cbd5adfc332d1133345723a5ac0512
sggnology <sggnology@gmail.com>
2025-05-31 10:54:54 +0900

    [master] client: 컨텐츠 좌측에 붙어서 노출되게끔 수정

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -130,7 +130,7 @@ function Home() {
           display: 'flex',
           flexWrap: 'wrap',
           gap: 3,
-          justifyContent: 'center',
+          justifyContent: 'start',
         }}
       >
         {content.map((item) => (
