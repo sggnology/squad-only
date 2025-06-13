e35c07bf5f651e1d2c47ed1bd3f437d1d84e42fe
sggnology <sggnology@gmail.com>
2025-06-13 10:43:49 +0900

    [master] server: UserInfo Documented -> MustBeDocumented annotation 으로 수정

--- server/src/main/kotlin/com/sggnology/server/common/annotation/UserInfoA.kt
+++ server/src/main/kotlin/com/sggnology/server/common/annotation/UserInfoA.kt
@@ -1,12 +1,6 @@
 package com.sggnology.server.common.annotation
 
-import java.lang.annotation.Documented
-import kotlin.annotation.Retention
-import kotlin.annotation.AnnotationRetention
-import kotlin.annotation.Target
-import kotlin.annotation.AnnotationTarget
-
 @Target(AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.RUNTIME)
-@Documented
+@MustBeDocumented
 annotation class UserInfoA
